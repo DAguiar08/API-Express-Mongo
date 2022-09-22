85f83c7eb81bc6925a1a872812b6ec50d6dfb78f
Davi Aguiar <davibenribaguiar@gmail.com>
2022-09-22 17:41:32 -0300

    Adicionado filtros e throw errors

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -5,41 +5,67 @@ import ProdutoService from "../service/ProdutoService"
 const ProdutoController = {
 
     async index(req : Request, res : Response): Promise<Response> {
-        let produtos = await ProdutoModel.find()
-            return res.json(produtos)
+        try {
+            const payload = req.query
+            let produtoFiltrado = await ProdutoModel.find(payload).limit(50)
+                return res.status(200).json(produtoFiltrado)
+        } catch (error) {
+            return res.status(400).json({ error })
+        }
+        
     },
+
+    /*async findLowStock(req : Request, res : Response): Promise<Response> {
+        try {
+            const qtd_stock = req.query
+            let produtoFiltrado = await ProdutoModel.find(qtd_stock).limit(50)
+                return res.status(200).json(produtoFiltrado)
+        } catch (error) {
+            return res.status(400).json({ error })
+        }
+        
+    },*/
 
     async findById(req : Request, res : Response): Promise<Response> {
-        const { id } = req.params
-        let produto = await ProdutoModel.findById(id)
-            return res.json(produto)
+        try {
+            const { id } = req.params
+            let produto = await ProdutoModel.findById(id)
+                return res.json(produto)
+        } catch (error) {
+            return res.status(400).json({ error })
+        }
+        
     },
-
-   /* async create(req : Request, res : Response): Promise<Response> {
-        let produto = await ProdutoModel.create(req.body)
-            return res.json(produto)
-    },*/
 
     async create(req : Request, res : Response): Promise<Response> {
         try {
-          const { titulo, descricao, departamento, marca, price, qtd_stock, bar_codes } = req.body;
-          const result = await ProdutoService.create({ titulo, descricao, departamento, marca, price, qtd_stock, bar_codes });
-          return res.status(201).json(result);
+            const { titulo, descricao, departamento, marca, price, qtd_stock, bar_codes } = req.body;
+            const result = await ProdutoService.create({ titulo, descricao, departamento, marca, price, qtd_stock, bar_codes });
+                return res.status(201).json(result);
         } catch (error) {
           return res.status(500).json({ error });
         }
       },
 
     async update(req : Request, res : Response): Promise<Response> {
-        const { id } = req.params
+        try {
+            const { id } = req.params
         let produto = await ProdutoModel.findByIdAndUpdate(id, req.body)
             return res.json(`Produto ${id} alterado!`)
+        } catch (error) {
+            return res.status(400).json({ error })
+        }
+        
     },
 
     async delete(req : Request, res : Response): Promise<Response> {
-        const { id } = req.params
-        let produto = await ProdutoModel.findByIdAndDelete(id)
-            return res.json(`Produto Deletado!`)
+        try {
+            const { id } = req.params
+                let produto = await ProdutoModel.findByIdAndDelete(id)
+            return res.status(204).json()
+        } catch (error) {
+            return res.status(400).json({ error })
+        }
     },
 }
 
