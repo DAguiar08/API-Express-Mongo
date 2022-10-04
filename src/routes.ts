66f46b325dce6deccc29981b68cc76fbfb50a8b6
Davi Aguiar <davibenribaguiar@gmail.com>
2022-10-04 09:41:38 -0300

    Refactor: Changed the portuguese things to english

--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,13 @@ import createValidation from "./validations/validator";
 const router = Router();
 
 router
-  .get("/produtos", ProductController.index) //Pode Buscar todos assim como receber filtros
-  .get("/produto/:id", ProductController.findById) //Busca por ID
-  .get("/produtos/lowstock", ProductController.findLowStock)
-  .post("/produto", createValidation, ProductController.create)
-  .post("/produtos/createCSV", ProductController.criaCsv)
-  .put("/produto/:id", createValidation, ProductController.update)
-  .patch("/produto/parcial/:id", ProductController.updateOne)
-  .delete("/produto/:id", ProductController.delete);
+  .get("/products", ProductController.index) //Pode Buscar todos assim como receber filtros
+  .get("/product/:id", ProductController.findById) //Busca por ID
+  .get("/product/lowstock", ProductController.findLowStock)
+  .post("/product", createValidation, ProductController.create)
+  .post("/products/createCSV", ProductController.criaCsv)
+  .put("/product/:id", createValidation, ProductController.update)
+  .patch("/product/parcial/:id", ProductController.updateOne)
+  .delete("/product/:id", ProductController.delete);
 
 export default router;
