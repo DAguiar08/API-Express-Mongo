53614c71958557c7620d89afd3fea7e95f0f2fad
Davi Aguiar <davibenribaguiar@gmail.com>
2022-10-05 18:27:31 -0300

    feat: Added documentation to the project
    Documentation using Swagger

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
 import ProductController from "./controllers/ProductController";
-import createValidation from "./validations/validator";
-import patchValidation from "./validations/PATCHvalidator";
+import createValidation from "./validations/Validator"
+import patchValidation from "./validations/PatchValidator";
+import GetValidation from "./validations/GetValidation";
 
 const router = Router();
 
 router
-  .get("/products", ProductController.index) //Pode Buscar todos assim como receber filtros
-  .get("/product/:id", ProductController.findById) //Busca por ID
-  .get("/product/lowstock", ProductController.findLowStock)
+  .get("/products", GetValidation ,ProductController.index) //Pode Buscar todos assim como receber filtros
+  .get("/product/:id", GetValidation ,ProductController.findById) //Busca por ID
+  .get("/products/lowstock", GetValidation ,ProductController.findLowStock)
   .post("/product", createValidation, ProductController.create)
   .post("/products/createCSV", ProductController.createCsv)
   .put("/product/:id", createValidation, ProductController.update)
-  .patch("/product/parcial/:id", patchValidation, ProductController.updateOne)
+  .patch("/product/:id", patchValidation, ProductController.updateOne)
   .delete("/product/:id", ProductController.delete);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes";
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 mongoose
   .connect("mongodb+srv://daguiar:Aguiar88@cluster0.etixs0l.mongodb.net/test")
