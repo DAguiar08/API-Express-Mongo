e7e0b6e3608824a62e72ead38c413e3649d91e80
Davi Aguiar <davibenribaguiar@gmail.com>
2022-10-10 18:32:00 -0300

    feat: Added put validation

---
+++ src/app/validations/PutValidator.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const schema = Joi.object({
+      title: Joi.string().trim().required(),
+      description: Joi.string().trim().required(),
+      departament: Joi.string().trim().required(),
+      brand: Joi.string().trim().required(),
+      price: Joi.number().min(0.01).max(1000).required(),
+      qtd_stock: Joi.number().max(100000).required(),
+      bar_codes: Joi.string().length(13).trim(),
+      stock_control_enebled: Joi.boolean(),
+    });
+
+    const { error } = await schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      throw error;
+    }
+    return next();
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -3,16 +3,17 @@ import ProductController from "../app/controllers/ProductController";
 import createValidation from "../app/validations/Validator";
 import patchValidation from "../app/validations/PatchValidator";
 import GetValidation from "../app/validations/GetValidation";
+import PutValidator from "../app/validations/PutValidator";
 
 const router = Router();
 
 router
-  .get("/products", GetValidation, ProductController.index) //Pode Buscar todos assim como receber filtros
+  .get("/products", GetValidation, ProductController.index) //Pode Buscar todos assim como receber filtro
   .get("/product/:id", GetValidation, ProductController.findById) //Busca por ID
   .get("/products/lowstock", GetValidation, ProductController.findLowStock)
   .post("/product", createValidation, ProductController.create)
   .post("/products/createCSV", ProductController.createCsv)
-  .put("/product/:id", createValidation, ProductController.update)
+  .put("/product/:id", PutValidator, ProductController.update)
   .patch("/product/:id", patchValidation, ProductController.updateOne)
   .delete("/product/:id", ProductController.delete);
 
