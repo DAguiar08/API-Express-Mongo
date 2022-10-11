8e8b60da598e9edab592ed0438b06eb942ebe001
Davi Aguiar <davibenribaguiar@gmail.com>
2022-10-11 14:59:05 -0300

    feat: Added validation to csv

--- src/app/service/ProductService.ts
+++ src/app/service/ProductService.ts
@@ -3,9 +3,8 @@ import ProductRepository from "../repository/ProductRepository";
 import ProductModel from "../database/ProductModel";
 import { Request } from "express";
 import { readFile } from "fs/promises";
-import Joi from "joi";
 import { IProduct } from "../interfaces/ProductInterface";
-import ValidateCB from "../../utils/ValidateCB";
+import ValidateCB from "../utils/ValidateCB";
 import { ParsedQs } from "qs";
 
 class ProductService {
@@ -47,25 +46,12 @@ class ProductService {
   }
 
   async createCSV() {
-    const readingFile = (await readFile("test.csv")).toString();
+    const readingFile = (await readFile("test2.csv")).toString();
     const splitFile = readingFile.split("\r\n");
     const [header, ...files] = splitFile;
     const arr = [];
     const arr2 = [];
     const data = new Date();
-    const schema = Joi.array().items(
-      Joi.object({
-        title: Joi.string().required().trim(),
-        description: Joi.string().required().trim(),
-        departament: Joi.string().required().trim(),
-        brand: Joi.string().required().trim(),
-        price: Joi.number().min(0.01).max(1000).required(),
-        qtd_stock: Joi.number().max(100000).required(),
-        bar_codes: Joi.string().length(13).required().trim(),
-        stock_control_enebled: Joi.boolean(),
-        created_at: Joi.date(),
-      })
-    );
     for (const i of files) {
       const splitFiles = i.split(",");
       const ValidateQS = Number(splitFiles[5]);
@@ -73,7 +59,7 @@ class ProductService {
         bar_codes: splitFiles[6],
       });
       if (ValidateQS < 1) {
-        arr2.push({
+        arr.push({
           title: splitFiles[0],
           description: splitFiles[1],
           departament: splitFiles[2],
@@ -84,17 +70,11 @@ class ProductService {
           stock_control_enebled: Boolean(false),
           created_at: data,
         });
-        const { error } = await schema.validate(arr2, { abortEarly: false });
-        if (error) {
-          throw error;
-        }
         if (ValidateCB) {
           throw console.error("This code bars already exists");
-        } else {
-          await ProductRepository.createCSV(arr2);
         }
       } else {
-        arr.push({
+        arr2.push({
           title: splitFiles[0],
           description: splitFiles[1],
           departament: splitFiles[2],
@@ -105,17 +85,13 @@ class ProductService {
           stock_control_enebled: Boolean(true),
           created_at: data,
         });
-        const { error } = await schema.validate(arr, { abortEarly: false });
-        if (error) {
-          throw error;
-        }
         if (ValidateCB) {
           throw console.error("This code bars already exists");
-        } else {
-          await ProductRepository.createCSV(arr);
         }
       }
     }
+    const arr3 = [...arr, ...arr2]
+    await ProductRepository.createCSV(arr3);
   }
 }
 
