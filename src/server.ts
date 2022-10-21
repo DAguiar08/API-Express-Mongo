76fa333f49470aabac34868d3f89f7a24bcc2f45
Davi Aguiar <davibenribaguiar@gmail.com>
2022-10-21 18:38:12 -0300

    feat: Added .env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import App from "./app";
+import logger from "./app/loggers/Logger";
 
 const app = new App();
 
-app.init().listen(process.env.PORT || 3000, () => {
-  console.info(`App starting at http://localhost:3000`);
-  console.info(`Envs: ${process.env.TARGET || "local"}`);
+app.init().listen(process.env.PORT, () => {
+  logger.info(`App starting at http://localhost:${process.env.PORT}`);
+  logger.info(`Envs: ${process.env.TARGET}`);
 });
